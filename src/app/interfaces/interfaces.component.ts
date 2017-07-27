061db3df01bab8d1a4989c8a9939162697a22bd3
xiaodao2015 <wubikuaike@163.com>
2017-07-27 17:52:14 +0800

    删除测试文件

--- src/app/interfaces/interfaces.component.ts
+++ src/app/interfaces/interfaces.component.ts
@@ -10,6 +10,12 @@ export class InterfacesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    function printLabel(labelledObj: { label: string }) {
+      console.log(labelledObj.label);
+    }
+
+    let myObj = { size: 10, label: "Size 10 Object" };
+    printLabel(myObj);
   }
 
 }
